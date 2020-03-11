38e91dc978f789b53b1c78c1d3a26216eb452cd2
chinshuhui <61679592+chinshuhui@users.noreply.github.com>
2020-03-11 18:11:34 +0800

    Update LabEx2_Q4_RandomInRange.java

--- EXERCISES/WEEK 2/LabEx2_Q4_RandomInRange.java
+++ EXERCISES/WEEK 2/LabEx2_Q4_RandomInRange.java
@@ -1,18 +1,23 @@
 import java.util.Scanner;
 public class LabEx2_Q4_RandomInRange {
-   public static void main(String[] args){
-   Scanner sc = new Scanner(System.in);
-   
-   int start, finish, count = 0;
-   
-   System.out.print("Starting number?: ");
-      start = sc.nextInt();
-      
-   System.out.print("Finishing number?: ");
-      finish = sc.nextInt();
-      
-   for (int c = start; c <= finish; c++)  // A for loop counts steps so far
-         System.out.print(c + " "); 
-   
-   }
+    public static void main (String[] args)
+    {
+     Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Please enter a number as first number:");
+        int num1 = sc.nextInt();
+        System.out.println("Please enter a number as second number:");
+        int num2 = sc.nextInt();
+        
+        if (num1 > num2)
+        {
+            System.out.println("0");
+        }
+        else
+        {
+            int x = (int)(Math.random()*((num2-num1)+1))+num1;
+            
+            System.out.println("The random integer is " +x);
+        }
+    }
 }
