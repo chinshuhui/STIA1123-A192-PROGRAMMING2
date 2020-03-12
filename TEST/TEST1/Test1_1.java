585a3c0f01bd5c3a6eb614c639c938a0cbfc5dce
chinshuhui <61679592+chinshuhui@users.noreply.github.com>
2020-03-12 15:11:58 +0800

    Add files via upload

---
+++ TEST/TEST1/Test1_1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Test1_1
+{
+public void dispMessage()
+    {
+        System.out.println("Hello World.");
+    }
+     
+    //Main method
+    public static void main(String s[])
+    {
+        //creat object of HelloWorld Class
+        HelloWorld obj=new HelloWorld();
+        obj.dispMessage();
+    }
+}
