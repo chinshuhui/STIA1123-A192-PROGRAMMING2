e0edb097fe49b9edfa2bb25b86b3657ed6379ee6
chinshuhui <61679592+chinshuhui@users.noreply.github.com>
2020-03-12 15:26:32 +0800

    Add files via upload

---
+++ TEST/TEST1/Test1_2.java
@@ -0,0 +1,23 @@
+public class Test1_2
+ {
+   static byte val1;
+   static short val2;
+   static int val3;
+   static long val4;
+   static float val5;
+   static double val6;
+   static char val7;
+   static boolean val8;
+   
+   public static void main(String[] args) {
+      System.out.println("Default values.....");
+      System.out.println("byte = " + val1);
+      System.out.println("short =" + val2);
+      System.out.println("int = " + val3);
+      System.out.println("long = " + val4);
+      System.out.println("float = " + val5);
+      System.out.println("double = " + val6);
+      System.out.println("char = " +val7);
+      System.out.println("boolean = " + val8);
+}
+}
