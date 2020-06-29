6a07396cd0f3bd39ff9439337b9c89ce9299ec2c
chinshuhui <61679592+chinshuhui@users.noreply.github.com>
2020-06-29 11:41:35 +0800

    Add files via upload

---
+++ ASSIGMENT/ASSIGNMENT3/Flowers.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Acer
+ */
+public class Flowers {
+  String flowername,flowercolour,flowersuit;
+    
+  public Flowers (String fn,String fc,String fs){
+      this.flowername = fn;
+      this.flowercolour = fc;
+      this.flowersuit = fs;
+     
+  }
+  
+}
