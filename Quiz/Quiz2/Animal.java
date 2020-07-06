2c8bb6f32b3bfd3eca612209294ac0fecd0cbfed
chinshuhui <61679592+chinshuhui@users.noreply.github.com>
2020-07-06 16:20:17 +0800

    Add files via upload

---
+++ Quiz/Quiz2/Animal.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Acer
+ */
+public class Animal{
+
+    private String name;
+
+    void introduceYourself() {
+      System.out.println("Morr. I am an animal.");
+    }
+}
